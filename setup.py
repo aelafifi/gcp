d5624e1af9ddb0a45de16590fe7e74bc4e25c80f
Goffi <goffi@goffi.org>
2011-01-25 16:59:18 +0100

    installtion: install script first draft

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+import sys
+from os import path
+from glob import glob
+
+name = 'gcp'
+
+setup(name=name,
+      version='0.1.2',
+      description=u"Goffi's CoPier",
+      long_description=u'gcp is a command-line tool to copy files, loosely inspired from cp, but with high level functionalities such as progress bar, copy continuation on error, journaling to know which files were successfuly copied, name mangling to workaround filesystem limitations (FAT), unique copy queue, copy list managemet, command arguments close to cp',
+      author='Goffi (Jérôme Poisson)',
+      author_email='goffi@goffi.org',
+      url='http://wiki.goffi.org/wiki/Gcp',
+      classifiers=['Environment :: Console',
+                   'Intended Audience :: End Users/Desktop',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'
+                   ],
+      data_files=[(path.join(sys.prefix,'share/locale/fr/LC_MESSAGES'), ['i18n/fr/LC_MESSAGES/gcp.mo']),
+                  ('share/doc/%s/examples' % name, glob("examples/*.py")),
+                  ('share/doc/%s' % name, ['COPYING','COPYING.LESSER','README'])],
+      scripts=['gcp'],
+     )
