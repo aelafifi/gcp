2136a91df4ebe251f188c5bb6013214b8aa0acc7
Matteo Cypriani <mcy@lm7.fr>
2018-04-21 21:43:38 +0200

    Rework setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 from setuptools import setup
 import sys
@@ -7,22 +6,44 @@ from os import path
 
 name = 'gcp'
 
-setup(name=name,
-      version='0.1.4',
-      description=u"gcp is an advanced copy tool loosely inspired from cp",
-      long_description=u'gcp is a command-line tool to copy files, loosely inspired from cp, but with high level functionalities such as progress bar, copy continuation on error, journaling to know which files were successfuly copied, name mangling to workaround filesystem limitations (FAT), unique copy queue, copy list managemet, command arguments close to cp',
-      author='Goffi (Jérôme Poisson)',
-      author_email='goffi@goffi.org',
-      url='http://wiki.goffi.org/wiki/Gcp',
-      classifiers=['Environment :: Console',
-                   'Intended Audience :: End Users/Desktop',
-                   'License :: OSI Approved :: GNU General Public License (GPL)',
-                   'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'
-                   ],
-      data_files=[('share/locale/fr/LC_MESSAGES', ['i18n/fr/LC_MESSAGES/gcp.mo']),
-                  ('share/man/man1', ["gcp.1"]),
-                  ('share/doc/%s' % name, ['COPYING','README.md'])],
-      scripts=['gcp'],
-     )
+setup(
+    name=name,
+    version='0.2.0',
+    url='https://code.lm7.fr/mcy/gcp',
+    license='GPL-3+',
+
+    description="An advanced file copy tool loosely inspired from cp",
+    long_description_content_type='text/markdown',
+    long_description="""
+    **%s** is a command-line tool to copy files, loosely inspired from the `cp`
+    command, but with higher-level functionalities such as progress bar, copy
+    continuation on error, logging to know which files were successfully
+    copied, name mangling to workaround filesystem limitations (FAT), unique
+    copy queue, copy list management, etc.""" % name,
+    keywords='file copy',
+
+    author='Goffi (Jérôme Poisson)',
+    author_email='goffi@goffi.org',
+    maintainer='Matteo Cypriani',
+    maintainer_email='mcy@lm7.fr',
+
+    # Cf. https://pypi.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+
+    data_files=[
+        ('share/locale/fr/LC_MESSAGES', ['i18n/fr/LC_MESSAGES/gcp.mo']),
+        ('share/man/man1', ["gcp.1"]),
+        ('share/doc/%s' % name, ['COPYING', 'README.md']),
+    ],
+    scripts=['gcp'],
+    install_requires=['PyGObject', 'dbus-python'],
+    python_requires='>=3',
+)
